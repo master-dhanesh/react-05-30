b583df4d91f638e50c4a10edc9dbd9287ed53023
master-dhanesh <dhanesh1296@gmail.com>
2025-06-01 13:07:03 +0530

    Outsource axios | useEffect Hook | Lifecycle of Components

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,37 @@
-import axios from "axios";
+import { useEffect, useState } from "react";
+import axios from "../utils/axios";
 
 const Products = () => {
+    const [active, setactive] = useState(false);
+    const [products, setproducts] = useState(null);
+
     const GetProducts = async () => {
         try {
-            const res = await axios.get("https://fakestoreapi.com/products/");
-            console.log(res.data);
+            const res = await axios.get("/products/");
+            console.log(res);
+            setproducts(res);
         } catch (error) {
             console.log(error);
         }
     };
 
+    useEffect(() => {
+        console.log("Product.jsx | Mounted");
+        GetProducts();
+        return () => {
+            console.log("Product.jsx | Unmount");
+        };
+    }, [active]);
+
+    console.log(active);
+
     return (
         <div>
             <h1 className="text-5xl mb-3">Products</h1>
-            <button onClick={GetProducts}>Get Products</button>
+            <hr />
+            <button onClick={() => setactive(!active)}>
+                {active ? "Unactive" : "Active"}
+            </button>
         </div>
     );
 };
